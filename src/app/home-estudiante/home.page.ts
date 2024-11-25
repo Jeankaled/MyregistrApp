845416bc170d850067d9121cfa2d3585ac0c80be
jeankaled <jeanavilaauocuc.cl>
2024-11-25 02:53:37 -0300

    agrega scanner on "dev"

--- src/app/home-estudiante/home.page.ts
+++ src/app/home-estudiante/home.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { ModalController, NavController, ToastController } from '@ionic/angular';
 
 import { Platform } from '@ionic/angular';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
+import { LensFacing, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 
 
 
@@ -11,14 +13,50 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  
+  scanResult = "";
 
   constructor(
     private navCtrl : NavController,
     private plataform: Platform,
+    private modalController: ModalController,
+    private toastController: ToastController,
   ) { }
 
+
+
+  async Scaner() {
+    const modal = await this.modalController.create({
+    component: BarcodeScanningModalComponent,
+    cssClass: 'barcode-scanning-modal',
+    showBackdrop: false,
+    componentProps: { 
+      formats: 'QR_CODE',
+      LensFacing : LensFacing.Back,
+    }
+    });
+  
+    await modal.present();
+    const { data } = await modal.onDidDismiss();
+    if(data){ await modal.onWillDismiss();
+    this.scanResult = data?.barcode?.displayValue;
+    console.log("Presente");
+    this.presentToast("presente profe");
+   }else{
+    console.log("no presente");
+    this.presentToast("no presente");
+   }
+  
+  
+  }
+
   ngOnInit() {
+
+    if(this.plataform.is('capacitor')){
+
+      BarcodeScanner.isSupported().then();
+      BarcodeScanner.checkPermissions().then();
+      BarcodeScanner.removeAllListeners();
+    }
   }
 
   
@@ -29,7 +67,15 @@ export class HomePage implements OnInit {
     this.navCtrl.back();
   }
 
-
+  async presentToast(message:string, duration?:number){
+    const toast = await this.toastController.create(
+      {
+        message:message,
+        duration:duration?duration:2000
+      }
+    );
+    toast.present();
+  }
 
 
 
